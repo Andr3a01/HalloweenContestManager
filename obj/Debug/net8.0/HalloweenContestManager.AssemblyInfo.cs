9ce22682a848dea23fc06a7c058d1fe82b77b73d
a.sharoubim <andrea.sharoubim@twinhelix.eu>
2025-10-11 15:56:04 +0200

    First push branch 'Develop'

--- obj/Debug/net8.0/HalloweenContestManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/HalloweenContestManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HalloweenContestManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52bdff322701206174aa86c4a4bea132f960fed7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HalloweenContestManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HalloweenContestManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
